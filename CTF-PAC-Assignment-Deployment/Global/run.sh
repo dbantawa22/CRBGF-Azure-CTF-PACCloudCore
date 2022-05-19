6485447eb6a82273982c6410fbe13ad72a540dc5
Raoul Raghavan <raoul.raghavan@accenture.com>
2022-05-19 16:05:10 -0500

    updates to file names, policy set and assignment names, run.sh files, parameters, deleted log storage policy set and assignment

--- CTF-PAC-Assignment-Deployment/Global/run.sh
+++ CTF-PAC-Assignment-Deployment/Global/run.sh
@@ -21,11 +21,23 @@ connect_deployment()
     echo '------------------- Policy Assignments --------------------'
     echo $DEPLOYMENT_NAME
 
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-set-assignment-denyIP --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_denyIP.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_denyIP.parameters.json
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-set-assignment-network_traffic --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_network_traffic.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic.parameters.json
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-set-assignment-not_allowed_resource_types --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_not_allowed_resource_types.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_not_allowed_resource_types.parameters.json
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-set-assignment-network_traffic_logs --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_network_traffic_logs.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_logs.parameters.json
-    # az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-set-assignment-allowed_locations --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_allowed_locations.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_locations.parameters.json
+    ## Assignment - Network Traffic Logs ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-network-traffic-logs --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_network_traffic_logs.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_logs.parameters.json
+    
+    ## Assignment - Not Allowed Resource Types ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-not-allowed-resource-types --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_not_allowed_resource_types.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_not_allowed_resource_types.parameters.json
+    
+    ## Assignment - Deny Public IP ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-deny-Public-IP --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_deny_publicIP.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_deny_publicIP.parameters.json
+    
+    ## Assignment - Public Endpoints ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-public-endpoints --template-file ./CTF-PAC-Definition-Library/Policy-Assignments/assignment_DENY-PublicEndpoints.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DENY-PublicEndpoints.parameters.json
+    
+    ## Assignment - Network Traffic Controls ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-network-traffic-control --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_network_traffic_control.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_control.parameters.json
+    
+    ## Assignment - Allowed Locations ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-allowed-locations --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_allowed_locations.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_locations.parameters.json
 
     ## Assignment - KV ##
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Key-Vault --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_keyvault.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_keyvault.parameters.json
@@ -36,8 +48,8 @@ connect_deployment()
     ## Assignment - Tagging ##
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Tagging --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_tagging.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_tagging.parameters.json
 
-    ## Assignment - Diagnostic Settings ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-diagnostic-settings --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_diagnostic_settings.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_diagnostic_settings.parameters.json
+    ## Assignment - Diagnostic Settings to Event Hub ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Diagnostic-Settings-to-Event-Hub --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_diagnosticSettings_toEH.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_diagnosticSettings_toEH.parameters.json
 
     ## Assignment - VM Backup ##
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-VM-Backup --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_vm_backup.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_vm_backup.parameters.json
@@ -46,7 +58,7 @@ connect_deployment()
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-DDoS --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_ddos.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_ddos.parameters.json
 
     ## Assignment - Private DNS Zone ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Private-DNS-Zone --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_DINE-PrivateDNSZonesPolicySetDefinition.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DINE-PrivateDNSZonesPolicySetDefinition.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Private-DNS-Zone --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_DINE-PrivateDNSZones.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DINE-PrivateDNSZones.parameters.json
 
     ## Assignment - Deny IP Forwarding ##
     az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-assignment-Deny-IP-Forwarding --template-file ./CTF-PAC-Assignment-Library/Policy-Assignments/assignment_deny_ipforwarding.json
--- CTF-PAC-Definition-Library/run.sh
+++ CTF-PAC-Definition-Library/run.sh
@@ -29,61 +29,58 @@ connect_deployment()
     echo $DEPLOYMENT_NAME
     
     ## Policy Initiative - Network Traffic Logs ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-network-traffic-logs --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_network_traffic_logs.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_logs.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-network-traffic-logs --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_network_traffic_logs.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_logs.parameters.json
     
     ## Policy Initiative - Not Allowed Resource Types ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-not-allowed-resource-types --template-file  ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_not_allowed_resource_types.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_not_allowed_resource_types.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-not-allowed-resource-types --template-file  ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_not_allowed_resource_types.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_not_allowed_resource_types.parameters.json
     
     ## Policy Initiative - Deny Public IP ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-deny-Public-IP --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_deny_publicIP.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_deny_publicIP.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-deny-Public-IP --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_deny_publicIP.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_deny_publicIP.parameters.json
     
     ## Policy Initiative - Public Endpoints ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-public-endpoints --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/DENY-PublicEndpointsPolicySetDefinition.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DENY-PublicEndpointsPolicySetDefinition.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-public-endpoints --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_DENY-PublicEndpoints.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DENY-PublicEndpoints.parameters.json
     
     ## Policy Initiative - Network Traffic Control ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-network-traffic-control --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_network_traffic_control.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_control.parameters.json 
-    
-    ## Policy Initiative - Log Storage ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-log-storage --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_log_storage.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_log_storage.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-network-traffic-control --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_network_traffic_control.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_network_traffic_control.parameters.json 
     
     ## Policy Initiative - Allowed Locations ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-allowed-locations --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_allowed_locations.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_locations.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-allowed-locations --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_allowed_locations.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_locations.parameters.json
 
     ## Policy Initiative - KV ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Key-Vault --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_keyvault.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_keyvault.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Key-Vault --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_keyvault.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_keyvault.parameters.json
     
     ## Policy Initiative - VM Sizes ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Allowed-VM-SKUs --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_allowed_vm_sizes.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_vm_sizes.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Allowed-VM-SKUs --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_allowed_vm_sizes.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_allowed_vm_sizes.parameters.json
     
     ## Policy Initiative - Tagging ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Tagging --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_tagging.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_tagging.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Tagging --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_tagging.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_tagging.parameters.json
 
-    ## Policy Initiative - Diagnostic Settings ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-diagnostic-settings --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_diagnostic_settings.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_diagnostic_settings.parameters.json
+    ## Policy Initiative - Diagnostic Settings to Event Hub ##
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Diagnostic-Settings-to-Event-Hub --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_diagnosticSettings_toEH.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_diagnosticSettings_toEH.parameters.json
 
     ## Policy Initiative - VM Backup ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-VM-Backup --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_vm_backup.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_vm_backup.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-VM-Backup --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_vm_backup.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_vm_backup.parameters.json
 
     ## Policy Initiative - DDoS ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-DDoS --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_ddos.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_ddos.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-DDoS --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_ddos.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_ddos.parameters.json
     
     ## Policy Initiative - Private DNS Zone ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Private-DNS-Zone --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/DINE-PrivateDNSZonesPolicySetDefinition.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DINE-PrivateDNSZonesPolicySetDefinition.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Private-DNS-Zone --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_DINE-PrivateDNSZones.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_DINE-PrivateDNSZones.parameters.json
 
     ## Policy Initiative - Deny IP Forwarding ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Deny-IP-Forwarding --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_deny_ipforwarding.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Deny-IP-Forwarding --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_deny_ipforwarding.json
 
     ## Policy Initiative - TLS SSL Encryption in Transit ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-TLS-SSL-Encryption --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_TLS_SSL_Encryption.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_TLS_SSL_Encryption.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-TLS-SSL-Encryption --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_TLS_SSL_Encryption.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_TLS_SSL_Encryption.parameters.json
 
     ## Policy Initiative - Deny Audit Resources with Encryption ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-CMK-Encryption --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_CMK_Encryption.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_CMK_Encryption.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-CMK-Encryption --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_CMK_Encryption.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_CMK_Encryption.parameters.json
 
     ## Policy Initiative - Diagnostic Settings to LAW ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Diagnostic-Settings-to-LAW --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_diagnosticSettings_toLAW.json --CTF-PAC-Assignment-Deployment/Global ./Parameters/assignment_diagnosticSettings_toLAW.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Diagnostic-Settings-to-LAW --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_diagnosticSettings_toLAW.json --CTF-PAC-Assignment-Deployment/Global ./Parameters/assignment_diagnosticSettings_toLAW.parameters.json
 
     ## Policy Initiative - AG WAF ##
-    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Enforce-AG-WAF --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/policyset_enforce_AG_WAF.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_enforce_AG_WAF.parameters.json
+    az deployment mg create --location uksouth --management-group-id MGMT_ROOT --name policy-initiative-Enforce-AG-WAF --template-file ./CTF-PAC-Definition-Library/Policy-Initiatives/initiative_enforce_AG_WAF.json --parameters ./CTF-PAC-Assignment-Deployment/Global/assignment_enforce_AG_WAF.parameters.json
 
 }
 
