34ee6e839c9335fbea9d199c478b672bc2d74bb2
Raoul Raghavan <raoul.raghavan@accenture.com>
2022-05-20 14:51:09 -0500

    file updates, ready to test 1

--- CTF-PAC-Policy-Scripts/deletecustompolicy.ps1
+++ CTF-PAC-Policy-Scripts/deletecustompolicy.ps1
@@ -1,41 +1,49 @@
 #Login-AzAccount
-Connect-AzAccount -Tenant 'cf36141c-ddd7-45a7-b073-111f66d0b30c' -SubscriptionId '5ba7b7b9-5e6d-4e47-a765-ddf9054d1413'
-Set-AzContext -Subscription "5ba7b7b9-5e6d-4e47-a765-ddf9054d1413" -tenant "cf36141c-ddd7-45a7-b073-111f66d0b30c"
+
+Connect-AzAccount -SubscriptionId '4ce27a75-a843-43b4-99ed-006804c35d7e' -Tenant '2eaefbef-4530-46a0-9c3a-1bae7a45d64d'
+Set-AzContext -Subscription "4ce27a75-a843-43b4-99ed-006804c35d7e" -tenant "2eaefbef-4530-46a0-9c3a-1bae7a45d64d"
+
+#Delete Policy Assignments
 function Remove-PolicyAssignment {
     param(
         [bool][parameter(Mandatory)]$RemoveAll,
         [object]$PolicyAssignment,
-        [string][parameter(Mandatory)]$ManagmentGroupName 
-    ) 
+        [string][parameter(Mandatory)]$ManagmentGroupName,
+
+        [switch]$Verbose = $true
+    )
     
-    if ($RemoveAll -eq $true){
+    if ($RemoveAll -eq $true) {
     
-    $mgmtid = (Get-AzManagementGroup -GroupName $ManagmentGroupName).Id
-    $PolicyAssignments = Get-AzPolicyAssignment -Scope $mgmtid
-    foreach ($item in $PolicyAssignments) {
-        Remove-AzPolicyAssignment -Id $item.PolicyAssignmentId
-    }
-    }else{
-        #Remove Policy Def
-        foreach ($item in $PolicyAssignment){
-        $PolicyAssignment  = Get-AzPolicyAssignment -Name $item.Name
-        Remove-AzPolicyAssignment -Id $PolicyAssignment.ResourceId
+        $mgmtid = (Get-AzManagementGroup -GroupName $ManagmentGroupName).Id
+        $PolicyAssignments = Get-AzPolicyAssignment -Scope $mgmtid
+        foreach ($item in $PolicyAssignments) {
+            Remove-AzPolicyAssignment -Id $item.PolicyAssignmentId
         }
-        }     
-}  
+    }
+    else {
+        #Remove Policy Def
+        foreach ($item in $PolicyAssignment) {
+            $PolicyAssignment = Get-AzPolicyAssignment -Name $item.Name
+            Remove-AzPolicyAssignment -Id $PolicyAssignment.ResourceId
+        }
+    }     
+}
+
+#Delete Policy Initiatives
 function Remove-AzCustomPolicySet {
     param(
         [bool][parameter(Mandatory)]$RemoveAll,
         [string][parameter(Mandatory)]$ManagmentGroupName 
         #[object]$PolicySetList
     )
-    if ($RemoveAll -eq $true){
-    $PSet = Get-AzPolicySetDefinition -Custom -ManagementGroupName $ManagmentGroupName
-    #Remove Policy Set
-    foreach ($item in $PSet) {
-        Remove-AzPolicySetDefinition -Id $item.PolicySetDefinitionId -Force
-    }
-    <#}else{
+    if ($RemoveAll -eq $true) {
+        $PSet = Get-AzPolicySetDefinition -Custom -ManagementGroupName $ManagmentGroupName
+        #Remove Policy Set
+        foreach ($item in $PSet) {
+            Remove-AzPolicySetDefinition -Id $item.PolicySetDefinitionId -Force
+        }
+        <#}else{
     #Remove Policy Def
     foreach ($item in $PolicySetList){
     $PolicySet = Get-AzPolicyDefinition -Name $item.Name
@@ -44,6 +52,7 @@ function Remove-AzCustomPolicySet {
     } 
 } 
 
+#Delete Policy Definitions
 function Remove-AzCustomPolicyDefinition {
     param(
         [bool][parameter(Mandatory)]$RemoveAll,
@@ -51,13 +60,13 @@ function Remove-AzCustomPolicyDefinition {
         #[object]$PolicyDefinitionList
     )
 
-    if ($RemoveAll -eq $true){
-    $PDef = Get-AzPolicyDefinition -Custom -ManagementGroupName $ManagmentGroupName
-    #Remove Policy Def
-    foreach ($item in $PDef) {
-        Remove-AzPolicySetDefinition -Id $item.PolicyDefinitionId -Force
-    }
-    <#}else{
+    if ($RemoveAll -eq $true) {
+        $PDef = Get-AzPolicyDefinition -Custom -ManagementGroupName $ManagmentGroupName
+        #Remove Policy Def
+        foreach ($item in $PDef) {
+            Remove-AzPolicySetDefinition -Id $item.PolicyDefinitionId -Force
+        }
+        <#}else{
     #Remove Policy Def
     foreach ($item in $PolicyDefinitionList){
     $Policy = Get-AzPolicyDefinition -Name $item.Name
@@ -67,8 +76,13 @@ function Remove-AzCustomPolicyDefinition {
 } 
 #$PolicyDefinitionNameList = Import-Csv $env:USERPROFILE\desktop\PolicyDefinitionNameList.csv
 
-Remove-PolicyAssignment -RemoveAll $true -ManagmentGroupName 'testmanagementgroup'
-Remove-AzCustomPolicySet -RemoveAll $true -ManagmentGroupName 'testmanagementgroup'
-Remove-AzCustomPolicyDefinition -RemoveAll $true -ManagmentGroupName 'testmanagementgroup'
+Remove-PolicyAssignment
+Remove-AzCustomPolicySet
+Remove-AzCustomPolicyDefinition
 
 
+<#
+Remove-PolicyAssignment -RemoveAll $true -ManagmentGroupName 'MGMT_ROOT'
+Remove-AzCustomPolicySet -RemoveAll $true -ManagmentGroupName 'MGMT_ROOT'
+Remove-AzCustomPolicyDefinition -RemoveAll $true -ManagmentGroupName 'MGMT_ROOT'
+#>
